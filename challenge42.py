150005a1596a09ac72302aa9cc2f32b1b81c207a
carlosjorr <carlosjorr@outlook.com>
2023-09-06 13:11:18 -0700

    initial

---
+++ challenge42.py
@@ -0,0 +1,46 @@
+#!/usr/bin/python3
+
+import nmap
+
+scanner = nmap.PortScanner()
+
+print("Nmap Automation Tool")
+print("--------------------")
+
+ip_addr = input("IP address to scan: ")
+print("The IP you entered is: ", ip_addr)
+
+resp = input("""\nSelect scan to execute:
+                1) SYN ACK Scan
+                2) UDP Scan
+                3) Other Scan\n""")
+print("You have selected option: ", resp)
+
+port_range = '1-50'
+
+port_range = input("Enter port range (e.g., 1-100): ")
+
+if resp == '1':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sS')
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+elif resp == '2':
+    print("Nmap Version: ", scanner.nmap_version())
+    scanner.scan(ip_addr, port_range, '-v -sU')  # -sU for UDP scan
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['udp'].keys())
+elif resp == '3':
+    print("Nmap Version: ", scanner.nmap_version())
+    custom_args = input("Enter custom Nmap arguments: ")
+    scanner.scan(ip_addr, port_range, custom_args)
+    print(scanner.scaninfo())
+    print("Ip Status: ", scanner[ip_addr].state())
+    print(scanner[ip_addr].all_protocols())
+    print("Open Ports: ", scanner[ip_addr]['tcp'].keys())
+else:
+    print("Please enter a valid option.")
